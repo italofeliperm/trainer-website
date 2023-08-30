98e0cb32bd05d77a2bbdc4dabba9ea0772487724
italofeliperm <italoxbobs@hotmail.com>
2023-08-30 04:26:34 +0200

    metade-do-site

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
 
-menu.onclick = () => {
+menu.onclick = () =>{
     menu.classList.toggle('fa-times');
-}
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+
+
+var swiper = new Swiper(".home-slider",{
+    speceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
